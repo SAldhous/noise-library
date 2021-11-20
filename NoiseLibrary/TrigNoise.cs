b3b9a3b61fe52c0a752afb2c2606c8c8b41b0c42
SAldhous <74025271+SAldhous@users.noreply.github.com>
2021-11-20 19:13:41 +0000

    Amended the other eval functions

--- NoiseLibrary/TrigNoise.cs
+++ NoiseLibrary/TrigNoise.cs
@@ -107,13 +107,31 @@ namespace NoiseLibrary
             double val = 0.0;
             double maxVal = 0.0;
 
+            Vector3 v = new Vector3((float)x, (float)y, (float)z);
+            int seed = v.ToString().GetHashCode();
+
+            Random rng = new Random(seed);
+
             for (int i = 0; i < octaves; i++)
             {
-                val += Noise(new double[3]{x * frequency, y * frequency, z * frequency}, i) * amplitude;
-                maxVal += amplitude;
+                double valX = SampleFunction(x * frequency, 0, i) * amplitude;
+                double valY = SampleFunction(y * frequency, 1, i) * amplitude;
+                double valZ = SampleFunction(z * frequency, 2, i) * amplitude;
+
+                val += (valX + valY + valZ) / 3.0;
+
+                double maxAmp = amplitudes[0][i];
+                for (int axis = 0; axis < 3; axis++)
+                {
+                    if (amplitudes[axis][i] > maxAmp) maxAmp = amplitudes[axis][i];
+                }
+
+                maxVal += amplitude * maxAmp;
 
                 amplitude *= persistence;
-                frequency *= 2;
+
+                frequency *= RandomDouble(rng, 1.5, 2.5);
+                frequency += RandomDouble(rng, -0.2, 0.2);
             }
 
             return val / maxVal;
@@ -126,13 +144,32 @@ namespace NoiseLibrary
             double val = 0.0;
             double maxVal = 0.0;
 
+            Vector4 v = new Vector4((float)x, (float)y, (float)z, (float)w);
+            int seed = v.ToString().GetHashCode();
+
+            Random rng = new Random(seed);
+
             for (int i = 0; i < octaves; i++)
             {
-                val += Noise(new double[4]{x * frequency, y * frequency, z * frequency, w * frequency}, i) * amplitude;
-                maxVal += amplitude;
+                double valX = SampleFunction(x * frequency, 0, i) * amplitude;
+                double valY = SampleFunction(y * frequency, 1, i) * amplitude;
+                double valZ = SampleFunction(z * frequency, 2, i) * amplitude;
+                double valW = SampleFunction(w * frequency, 3, i) * amplitude;
+
+                val += (valX + valY + valZ + valW) / 4.0;
+
+                double maxAmp = amplitudes[0][i];
+                for (int axis = 0; axis < 4; axis++)
+                {
+                    if (amplitudes[axis][i] > maxAmp) maxAmp = amplitudes[axis][i];
+                }
+
+                maxVal += amplitude * maxAmp;
 
                 amplitude *= persistence;
-                frequency *= 2;
+
+                frequency *= RandomDouble(rng, 1.5, 2.5);
+                frequency += RandomDouble(rng, -0.2, 0.2);
             }
 
             return val / maxVal;
@@ -146,40 +183,6 @@ namespace NoiseLibrary
             if (functionType[axis][octave] == 0) return Math.Sin((position * mults[axis][octave]) - (constants[axis][octave] * mults[axis][octave])) * amplitudes[axis][octave] * sign;
             else return Math.Cos((position * mults[axis][octave]) - (constants[axis][octave] * mults[axis][octave])) * amplitudes[axis][octave] * sign;
         }
-
-        // Evaluate axes with respect to input axes
-        public static double Noise(double[] components, int octave)
-        {
-            double val = 0.0;
-            double maxVal = 0.0;
-
-            for (int i = 0; i < components.Length; i++)
-            {
-                if (functionType[i][octave] == 0) val += Math.Sin((components[i] * mults[i][octave]) - (constants[i][octave] * mults[i][octave])) * amplitudes[i][octave];
-                else val += Math.Cos((components[i] * mults[i][octave]) - (constants[i][octave] * mults[i][octave])) * amplitudes[i][octave];
-
-                maxVal += amplitudes[i][octave];
-            }
-
-            return val / maxVal;
-        }
-        /*public static double Noise(double x, double y, double z, int octave)
-        {
-            double xVal = Math.Sin((x * mults[0][octave]) + constants[0][octave]);
-            double yVal = Math.Sin((y * mults[1][octave]) + constants[1][octave]);
-            double zVal = Math.Sin((z * mults[2][octave]) + constants[2][octave]);
-
-            return (xVal + yVal + zVal) / 3.0;
-        }
-        public static double Noise(double x, double y, double z, double w, int octave)
-        {
-            double xVal = Math.Sin((x * mults[0][octave]) + constants[0][octave]);
-            double yVal = Math.Sin((y * mults[1][octave]) + constants[1][octave]);
-            double zVal = Math.Sin((z * mults[2][octave]) + constants[2][octave]);
-            double wVal = Math.Sin((w * mults[3][octave]) + constants[3][octave]);
-
-            return (xVal + yVal + zVal + wVal) / 4.0;
-        }*/
 
         // Taken from Adrian's Soapbox at https://adrianb.io/2014/08/09/perlinnoise.html
         public static double Fade(double t)
