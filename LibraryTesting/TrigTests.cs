24e49f69cfe628840688c70369699bee4395065c
SAldhous <74025271+SAldhous@users.noreply.github.com>
2021-11-21 20:42:17 +0000

    Worley noise success
    I succeeded in creating a continuous Worley noise algorithm. It sample surrounding cells with respect to the sampling point. At first I used a seeded random point based on position but it was slow. I took inspiration from the simplex algorithm and randomly computed points at startup. Then I used a hash method to figure out which point a cell contains. Managed to use up to 90 octaves on 48x48 grid in real-time.

---
+++ LibraryTesting/TrigTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoiseLibrary;
+
+namespace LibraryTesting
+{
+    [TestClass]
+    public class TrigTests
+    {
+        [TestMethod]
+        public void Evaluate4D()
+        {
+            bool flag = true;
+
+            try
+            {
+                TrigNoise.Generate(5);
+
+                for (int w = 0; w < 10; w++)
+                {
+                    for (int z = 0; z < 10; z++)
+                    {
+                        for (int y = 0; y < 10; y++)
+                        {
+                            for (int x = 0; x < 10; x++)
+                            {
+                                double val = TrigNoise.Evaluate(x, y, z, w, 0.3, 1.0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                flag = false;
+            }
+
+            Assert.IsTrue(flag);
+        }
+
+        [TestMethod]
+        public void Evaluate3D()
+        {
+            bool flag = true;
+
+            try
+            {
+                TrigNoise.Generate(5);
+
+                for (int z = 0; z < 10; z++)
+                {
+                    for (int y = 0; y < 10; y++)
+                    {
+                        for (int x = 0; x < 10; x++)
+                        {
+                            double val = TrigNoise.Evaluate(x, y, z, 0.3, 1.0);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                flag = false;
+            }
+
+            Assert.IsTrue(flag);
+        }
+
+        [TestMethod]
+        public void Evaluate2D()
+        {
+            bool flag = true;
+
+            try
+            {
+                TrigNoise.Generate(5);
+
+                for (int y = 0; y < 10; y++)
+                {
+                    for (int x = 0; x < 10; x++)
+                    {
+                        double val = TrigNoise.Evaluate(x, y, 0.3, 1.0);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                flag = false;
+            }
+
+            Assert.IsTrue(flag);
+        }
+    }
+}
