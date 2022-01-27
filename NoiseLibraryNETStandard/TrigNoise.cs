31e74a79c1a066ac501d4ac56ebdee90509bce6a
SAldhous <74025271+SAldhous@users.noreply.github.com>
2022-01-27 16:31:23 +0000

    Conversion to Double
    To keep things consistent, accurate and provide numerical range, I converted to data types into doubles where possible.

---
+++ NoiseLibraryNETStandard/TrigNoise.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace NoiseLibrary
+{
+    public class TrigNoise
+    {
+        private static Random RNG = new Random();
+        
+        // Array indices correspond to each axis X, Y, Z, and W
+        private static List<double>[] constants = new List<double>[4];
+        private static List<double>[] mults = new List<double>[4];
+        private static List<double>[] amplitudes = new List<double>[4];
+        private static List<int>[] signs = new List<int>[4];
+        private static List<int>[] functionType = new List<int>[4];
+
+        // Frequencies for each octave
+        private static List<double> frequencies = new List<double>();
+
+        private static int octaves;
+
+        /// <summary>
+        /// Generate the axes of noise using sin functions and constants
+        /// </summary>
+        /// <param name="octavesIn">Octaves to create</param>
+        public static void Generate(int octavesIn)
+        {
+            octaves = octavesIn;
+
+            constants = new List<double>[4];
+            mults = new List<double>[4];
+            amplitudes = new List<double>[4];
+            signs = new List<int>[4];
+            functionType = new List<int>[4];
+            frequencies = new List<double>();
+
+            double frequency = 1.0;
+
+            for (int j = 0; j < octaves; j++)
+            {
+                frequencies.Add(frequency);
+                frequency *= RandomDouble(1.93, 2.07);
+                frequency += RandomDouble(0.0, 0.1);
+            }
+
+            for (int i = 0; i < constants.Length; i++)
+            {
+                constants[i] = new List<double>();
+                mults[i] = new List<double>();
+                amplitudes[i] = new List<double>();
+                signs[i] = new List<int>();
+                functionType[i] = new List<int>();
+
+                for (int j = 0; j < octaves; j++)
+                {
+                    constants[i].Add(RandomDouble(2 * -Math.PI, 2 * Math.PI));
+                    mults[i].Add(RandomDouble(1.5, 3.0));
+                    amplitudes[i].Add(RandomDouble(0.2, 1.0));
+                    signs[i].Add(RNG.Next(0, 2));
+                    functionType[i].Add(RNG.Next(0, 2));
+                }
+            }
+        }
+        
+        // Evaluate noise at (x, y) for all octaves specified starting at defined frequency
+        public static double Evaluate(double x, double y, double persistence, double initialFreq)
+        {
+            double frequency = initialFreq;
+            double amplitude = 1.0;
+
+            double val = 0.0;
+            double maxVal = 0.0;
+
+            Vector2 v = new Vector2((float)x, (float)y);
+            int seed = v.ToString().GetHashCode();
+
+            Random rng = new Random(seed);
+
+            for (int i = 0; i < octaves; i++)
+            {
+                double valX = SampleFunction(x * frequency, 0, i) * amplitude;
+                double valY = SampleFunction(y * frequency, 1, i) * amplitude;
+
+                val += (valX + valY) / 2.0;
+
+                double maxAmp = amplitudes[0][i];
+                for (int axis = 0; axis < 2; axis++)
+                {
+                    if (amplitudes[axis][i] > maxAmp) maxAmp = amplitudes[axis][i];
+                }
+
+                maxVal += amplitude * maxAmp;
+
+                amplitude *= persistence;
+
+                frequency *= RandomDouble(rng, 1.5, 2.5);
+                frequency += RandomDouble(rng, -0.2, 0.2);
+            }
+
+            return val / maxVal;
+        }
+        public static double Evaluate(double x, double y, double z, double persistence, double initialFreq)
+        {
+            double frequency = initialFreq;
+            double amplitude = 1.0;
+
+            double val = 0.0;
+            double maxVal = 0.0;
+
+            Vector3 v = new Vector3((float)x, (float)y, (float)z);
+            int seed = v.ToString().GetHashCode();
+
+            Random rng = new Random(seed);
+
+            for (int i = 0; i < octaves; i++)
+            {
+                double valX = SampleFunction(x * frequency, 0, i) * amplitude;
+                double valY = SampleFunction(y * frequency, 1, i) * amplitude;
+                double valZ = SampleFunction(z * frequency, 2, i) * amplitude;
+
+                val += (valX + valY + valZ) / 3.0;
+
+                double maxAmp = amplitudes[0][i];
+                for (int axis = 0; axis < 3; axis++)
+                {
+                    if (amplitudes[axis][i] > maxAmp) maxAmp = amplitudes[axis][i];
+                }
+
+                maxVal += amplitude * maxAmp;
+
+                amplitude *= persistence;
+
+                frequency *= RandomDouble(rng, 1.5, 2.5);
+                frequency += RandomDouble(rng, -0.2, 0.2);
+            }
+
+            return val / maxVal;
+        }
+        public static double Evaluate(double x, double y, double z, double w, double persistence, double initialFreq)
+        {
+            double frequency = initialFreq;
+            double amplitude = 1.0;
+
+            double val = 0.0;
+            double maxVal = 0.0;
+
+            Vector4 v = new Vector4((float)x, (float)y, (float)z, (float)w);
+            int seed = v.ToString().GetHashCode();
+
+            Random rng = new Random(seed);
+
+            for (int i = 0; i < octaves; i++)
+            {
+                double valX = SampleFunction(x * frequency, 0, i) * amplitude;
+                double valY = SampleFunction(y * frequency, 1, i) * amplitude;
+                double valZ = SampleFunction(z * frequency, 2, i) * amplitude;
+                double valW = SampleFunction(w * frequency, 3, i) * amplitude;
+
+                val += (valX + valY + valZ + valW) / 4.0;
+
+                double maxAmp = amplitudes[0][i];
+                for (int axis = 0; axis < 4; axis++)
+                {
+                    if (amplitudes[axis][i] > maxAmp) maxAmp = amplitudes[axis][i];
+                }
+
+                maxVal += amplitude * maxAmp;
+
+                amplitude *= persistence;
+
+                frequency *= RandomDouble(rng, 1.5, 2.5);
+                frequency += RandomDouble(rng, -0.2, 0.2);
+            }
+
+            return val / maxVal;
+        }
+
+        public static double SampleFunction(double position, int axis, int octave)
+        {
+            double sign = 1.0;
+            if (signs[axis][octave] != 0) sign = -1.0;
+
+            if (functionType[axis][octave] == 0) return Math.Sin((position * mults[axis][octave]) - (constants[axis][octave] * mults[axis][octave])) * amplitudes[axis][octave] * sign;
+            else return Math.Cos((position * mults[axis][octave]) - (constants[axis][octave] * mults[axis][octave])) * amplitudes[axis][octave] * sign;
+        }
+
+        // Taken from Adrian's Soapbox at https://adrianb.io/2014/08/09/perlinnoise.html
+        public static double Fade(double t)
+        {
+            // Fade function as defined by Ken Perlin.  This eases coordinate values
+            // so that they will ease towards integral values.  This ends up smoothing
+            // the final output.
+            return t * t * t * (t * (t * 6 - 15) + 10);         // 6t^5 - 15t^4 + 10t^3
+        }
+
+        public static double RandomDouble(double min, double max)
+        {
+            double randomVal = RNG.NextDouble();
+            return min + randomVal * (max - min);
+        }
+        public static double RandomDouble(Random rng, double min, double max)
+        {
+            double randomVal = rng.NextDouble();
+            return min + randomVal * (max - min);
+        }
+    }
+}
---
+++ NoiseLibraryNETStandard/WorleyNoise.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace NoiseLibrary
+{
+    /// <summary>
+    /// The way this worley noise algorithm works is by take the sample point and working out what cell it belongs to in order to find the worley points of the surrounding cells.
+    /// The worley point for each cell is determined by a hash value, created by bitwise operations on the coordinates of the cell and added together. A modulus operation is performed
+    /// on the total sum of the bitwise operations. The final result is an index value for which point to use. The noise function then returns the square route (performed at return)
+    /// of the shortest distance.
+    /// </summary>
+    public class WorleyNoise
+    {
+        private static Random RNG = new Random();
+
+        private static Vector<double>[] grad4 = new Vector<double>[32];
+
+        // Array of perminations
+        private static short[] p = {151,160,137,91,90,15,
+        131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+        190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+        88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+        77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+        102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+        135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+        5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+        223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+        129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+        251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+        49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+        138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
+        // To remove the need for index wrapping, double the permutation table length
+        private static short[] perm = new short[512];
+        private static short[] permMod12 = new short[512];
+
+        // Initiate the noise but creating random vectors for cells
+        // The hash table tells the algorithm which point to use for a given cell
+        // The hash values are naively obtained using bitwise operators and take a modulus of the sum of thos operations
+        public static void Init()
+        {
+            for (int i = 0; i < 512; i++)
+            {
+                perm[i] = p[i & 255];
+                permMod12[i] = (short)(perm[i] % 12);
+            }
+
+            for (int i = 0; i < 32; i++)
+            {
+                double x = RNG.NextDouble();
+                double y = RNG.NextDouble();
+                double z = RNG.NextDouble();
+                double w = RNG.NextDouble();
+
+                grad4[i] = new Vector<double>(new double[4] { x, y, z, w } );
+            }
+        }
+
+        public static double Evaluate(double x, double y, int octaves, double persistence, double initialFreq)
+        {
+            double frequency = initialFreq;
+            double amplitude = 1.0;
+
+            double val = 0.0;
+            double maxVal = 0.0;
+
+            for (int i = 0; i < octaves; i++)
+            {
+                val += Clamp(Noise(x * frequency, y * frequency), 0.0, 1.0) * amplitude;
+                maxVal += amplitude;
+
+                amplitude *= persistence;
+                frequency *= 2;
+            }
+
+            return val / maxVal;
+        }
+
+        public static double Evaluate(double x, double y, double z, int octaves, double persistence, double initialFreq)
+        {
+            double frequency = initialFreq;
+            double amplitude = 1.0;
+
+            double val = 0.0;
+            double maxVal = 0.0;
+
+            for (int i = 0; i < octaves; i++)
+            {
+                val += Clamp(Noise(x * frequency, y * frequency, z * frequency), 0.0, 1.0) * amplitude;
+                maxVal += amplitude;
+
+                amplitude *= persistence;
+                frequency *= 2;
+            }
+
+            return val / maxVal;
+        }
+
+        public static double Evaluate(double x, double y, double z, double w, int octaves, double persistence, double initialFreq)
+        {
+            double frequency = initialFreq;
+            double amplitude = 1.0;
+
+            double val = 0.0;
+            double maxVal = 0.0;
+
+            for (int i = 0; i < octaves; i++)
+            {
+                val += Clamp(Noise(x * frequency, y * frequency, z * frequency, w * frequency), 0.0, 1.0) * amplitude;
+                maxVal += amplitude;
+
+                amplitude *= persistence;
+                frequency *= 2;
+            }
+
+            return val / maxVal;
+        }
+
+        public static double Noise(double x, double y)
+        {
+            Vector<double> sampleCoords = new Vector<double>(new double[4] { x, y, 0.0, 0.0 });
+
+            double shortest = double.MaxValue;
+
+            for (int Y = -1; Y <= 1; Y++)
+            {
+                for (int X = -1; X <= 1; X++)
+                {
+                    double flooredX = Math.Floor(x + X);
+                    double flooredY = Math.Floor(y + Y);
+
+                    int i = (int)flooredX & 255;
+                    int j = (int)flooredY & 255;
+
+                    int hash = (perm[i] + perm[j]) % 32;
+                    Vector<double> v = grad4[hash] + new Vector<double>(new double[4] { flooredX, flooredY, 0.0, 0.0} );
+
+                    Vector<double> dist = v - sampleCoords;
+                    double distSqr = (dist[0] * dist[0]) + (dist[1] * dist[1]);
+
+                    if (distSqr < shortest) shortest = distSqr;
+                }
+            }
+
+            return Math.Sqrt(shortest);
+        }
+
+        public static double Noise(double x, double y, double z)
+        {
+            Vector<double> sampleCoords = new Vector<double>(new double[4] { x, y, z, 0.0 });
+
+            double shortest = double.MaxValue;
+
+            for (int Z = -1; Z <= 1; Z++)
+            {
+                for (int Y = -1; Y <= 1; Y++)
+                {
+                    for (int X = -1; X <= 1; X++)
+                    {
+                        double flooredX = Math.Floor(x + X);
+                        double flooredY = Math.Floor(y + Y);
+                        double flooredZ = Math.Floor(z + Z);
+
+                        int i = (int)flooredX & 255;
+                        int j = (int)flooredY & 255;
+                        int k = (int)flooredZ & 255;
+
+                        int hash = (perm[i] + perm[j] + perm[k]) % 32;
+                        Vector<double> v = grad4[hash] + new Vector<double>(new double[4] { flooredX, flooredY, flooredZ, 0.0 });
+
+                        Vector<double> dist = v - sampleCoords;
+                        double distSqr = (dist[0] * dist[0]) + (dist[1] * dist[1]) + (dist[2] * dist[2]);
+
+                        if (distSqr < shortest) shortest = distSqr;
+                    }
+                }
+            }
+
+            return Math.Sqrt(shortest);
+        }
+
+        public static double Noise(double x, double y, double z, double w)
+        {
+            Vector<double> sampleCoords = new Vector<double>(new double[4] { x, y, z, w });
+
+            double shortest = double.MaxValue;
+
+            for (int W = -1; W <= 1; W++)
+            {
+                for (int Z = -1; Z <= 1; Z++)
+                {
+                    for (int Y = -1; Y <= 1; Y++)
+                    {
+                        for (int X = -1; X <= 1; X++)
+                        {
+                            double flooredX = Math.Floor(x + X);
+                            double flooredY = Math.Floor(y + Y);
+                            double flooredZ = Math.Floor(z + Z);
+                            double flooredW = Math.Floor(w + W);
+
+                            int i = (int)flooredX & 255;
+                            int j = (int)flooredY & 255;
+                            int k = (int)flooredZ & 255;
+                            int l = (int)flooredW & 255;
+
+                            int hash = (perm[i] + perm[j] + perm[k] + perm[l]) % 32;
+                            Vector<double> v = grad4[hash] + new Vector<double>(new double[4] { flooredX, flooredY, flooredZ, flooredW });
+
+                            Vector<double> dist = v - sampleCoords;
+                            double distSqr = (dist[0] * dist[0]) + (dist[1] * dist[1]) + (dist[2] * dist[2]) + (dist[3] * dist[3]);
+
+                            if (distSqr < shortest) shortest = distSqr;
+                        }
+                    }
+                }
+            }
+
+            return Math.Sqrt(shortest);
+        }
+
+        public static double Clamp(double val, double min, double max)
+        {
+            if (val > max)
+                return max;
+            if (val < min)
+                return min;
+
+            return val;
+        }
+    }
+}
